b6e181c1decf4cb4c9a8313c763b0619b021572b
Your Name <juanfranciscofernandezherreros>
2023-04-13 23:40:50 +0200

    match

--- src/main/java/com/basketball/match/consumer/Consumer.java
+++ src/main/java/com/basketball/match/consumer/Consumer.java
@@ -1,5 +1,6 @@
 package com.basketball.match.consumer;
 
+import com.basketball.match.events.Event;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
@@ -7,11 +8,11 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+import java.util.Objects;
 
 @Slf4j
 @Component
 public class Consumer {
-    private static final String orderTopic = "${topic.name}";
     private final ObjectMapper objectMapper;
     private final ModelMapper modelMapper;
 
@@ -21,9 +22,17 @@ public class Consumer {
         this.modelMapper = modelMapper;
     }
 
-    @KafkaListener(topics = orderTopic)
-    public void consumeMessage(String message) throws JsonProcessingException {
-        log.info("message consumed {}", message);
+    @KafkaListener(
+            topics = {"${topic.name}"},
+            groupId = "${kafka.group}",
+            containerFactory = "kafkaListenerContainerFactory"
+    )
+    public void listenEvents(Event event){
+        if (Objects.nonNull(event.getPayload().getSource())){
+            log.info("New event consumed successfully");
+        }else{
+            log.error("The event has been discarded for not having the expected information");
+        }
     }
 
 }
---
+++ src/main/java/com/basketball/match/events/EventSource.java
@@ -0,0 +1,16 @@
+package com.basketball.match.events;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EventSource {
+
+    private String table;
+
+}
